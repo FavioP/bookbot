a5a16fe3dfa4ce144cc64f0ea593d316990eb2c4
FavioP <favio.p.96@gmail.com>
2025-10-07 18:12:06 +0000

    updated main.py and stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counter
+from stats import word_counter, char_counter, sort_dict, output_formatted
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,7 +7,10 @@ def get_book_text(filepath):
 
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
+    book_text = book_text.lower()
     word_count = word_counter(book_text)
-    print(f"Found {word_count} total words")
+    letter_count = char_counter(book_text)
+    letter_count = sort_dict(letter_count)
+    output_formatted(word_count,letter_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,38 @@
 #A file that contains functions for analyzing the text
 
-def word_counter(words):
-    tmp = words.split()
-    return len(tmp)
+def get_num(dictionary):
+    return dictionary["num"]
+
+def word_counter(string):
+    words = string.split()
+    return len(words)
+
+def char_counter(string):
+    words = string.split()
+    characters = {}
+    for word in words:
+        for letter in word:
+            if letter in characters:
+                characters[letter] += 1
+            else:
+                characters[letter] = 1
+    return characters
+
+def sort_dict(letters):
+    char_counter = []
+    for key in letters:
+        tmp = {"char": key, "num": letters[key]}
+        char_counter.append(tmp)
+    char_counter = sorted(char_counter, key=get_num, reverse=True)
+    return char_counter
+
+def output_formatted(word_count,letters):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char in letters:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+    print("============= END ===============")
